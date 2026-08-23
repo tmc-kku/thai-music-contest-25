3444b606edd90cfb9ec547d814e16f433d80c0a2
sirisak <tmc.pa.kku@gmail.com>
2026-08-23 17:46:33 +0700

    Remove results link from navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -34,7 +34,6 @@ export default function Header() {
           <NavLink href="/">หน้าแรก</NavLink>
           <NavLink href="/schedule">กำหนดการ</NavLink>
           <NavLink href="/finalists">รายชื่อผู้เข้ารอบ</NavLink>
-          <NavLink href="/result">ผลการประกวด</NavLink>
           <NavLink href="/live">ถ่ายทอดสด</NavLink>
           <NavLink href="/venue">สถานที่</NavLink>
           <NavLink href="/contacts">ติดต่อ</NavLink>
@@ -68,7 +67,6 @@ export default function Header() {
             <NavLink href="/" onClick={() => setOpen(false)}>หน้าแรก</NavLink>
             <NavLink href="/schedule" onClick={() => setOpen(false)}>กำหนดการ</NavLink>
             <NavLink href="/finalists" onClick={() => setOpen(false)}>รายชื่อผู้เข้ารอบ</NavLink>
-            <NavLink href="/result" onClick={() => setOpen(false)}>ผลการประกวด</NavLink>
             <NavLink href="/live" onClick={() => setOpen(false)}>ถ่ายทอดสด</NavLink>
             <NavLink href="/venue" onClick={() => setOpen(false)}>สถานที่</NavLink>
             <NavLink href="/contacts" onClick={() => setOpen(false)}>ติดต่อ</NavLink>
