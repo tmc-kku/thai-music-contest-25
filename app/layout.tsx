429137cfbbd667d818faa56c39827d73da105853
tmc-kku <tmc.pa.kku@gmail.com>
2026-08-26 01:10:59 +0700

    Import DarkVeil component in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Metadata } from 'next'
+import DarkVeil from '@/components/DarkVeil'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
